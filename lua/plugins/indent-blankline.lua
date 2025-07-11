1ac5f283bd9b28947a958d27ced51cb301becf45
Lucas Miguel <lucas.miguel@dommus.tec.br>
2025-07-11 10:37:34 -0300

    Finalizado ajuste de themas

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -6,7 +6,9 @@ return {
         ---@type ibl.config
         opts = {},
         config = function ()
-          require("ibl").setup()
+          require("ibl").setup({
+            scope = { enabled = true }
+          })
         end
     }
 }
