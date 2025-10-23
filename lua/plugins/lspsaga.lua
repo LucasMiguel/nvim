b825e23d77c0a722bfd9e9cbf0fc9989f340d9dd
Lucas Miguel <lucas.miguel@dommus.tec.br>
2025-10-23 11:21:13 -0300

    Ajuste de formatação

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -1,7 +1,29 @@
 return {{
   'nvimdev/lspsaga.nvim',
   config = function()
-      require('lspsaga').setup({})
+      local function escape_winbar(str)
+          if not str then return "" end
+          -- Substitui os caracteres ilegais por alternativas seguras
+          return str:gsub("[%$<>]", function(c)
+              if c == "$" then return "＄"  -- símbolo seguro
+              elseif c == "<" then return "〈"
+              elseif c == ">" then return "〉"
+              end
+          end)
+      end
+      require('lspsaga').setup({
+        symbol_in_winbar = {
+            enable = false,
+            separator = " > ",
+            show_file = true,
+            formatter = function(symbol)
+                if symbol.name then
+                    symbol.name = escape_winbar(symbol.name)
+                end
+                return symbol
+            end,
+        },
+      })
   end,
   dependencies = {
       'nvim-treesitter/nvim-treesitter', -- optional
