b1d9663cfe2f5a315bd9abe64a0467734f205792
Lucas Miguel <lucas.miguel@dommus.tec.br>
2026-02-23 15:05:39 -0300

    Ajuste para funcionar com php

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,7 +7,6 @@ return {
       "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
-      -- Mason
       require("mason").setup()
 
       require("mason-lspconfig").setup({
@@ -17,55 +16,82 @@ return {
           "html",
           "cssls",
           "jsonls",
+          "intelephense", -- PHP
         },
       })
 
-      -- Capabilities (autocomplete)
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- Attach
       local on_attach = function(_, bufnr)
         local opts = { buffer = bufnr }
-
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
         vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
         vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
       end
 
-      -- TS / JS (ts_ls)
+      -- ========================
+      -- TS / JS
+      -- ========================
       vim.lsp.config("ts_ls", {
         capabilities = capabilities,
         on_attach = on_attach,
         cmd = {
           "/home/lucasmiguel/.config/nvm/versions/node/v24.13.1/bin/node",
-          "/home/lucasmiguel/.local/share/nvim/mason/packages/typescript-language-server/node_modules/typescript-language-server/lib/cli.mjs",
+          vim.fn.stdpath("data")
+            .. "/mason/packages/typescript-language-server/node_modules/typescript-language-server/lib/cli.mjs",
           "--stdio",
         },
       })
 
+      -- ========================
       -- Angular
+      -- ========================
       vim.lsp.config("angularls", {
         capabilities = capabilities,
         on_attach = on_attach,
       })
 
+      -- ========================
       -- HTML
+      -- ========================
       vim.lsp.config("html", {
         capabilities = capabilities,
         on_attach = on_attach,
       })
 
+      -- ========================
       -- CSS
+      -- ========================
       vim.lsp.config("cssls", {
         capabilities = capabilities,
         on_attach = on_attach,
       })
 
+      -- ========================
       -- JSON
+      -- ========================
       vim.lsp.config("jsonls", {
         capabilities = capabilities,
         on_attach = on_attach,
       })
+
+      -- ========================
+      -- PHP (Laravel)
+      -- ========================
+      vim.lsp.config("intelephense", {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
+
+      vim.lsp.enable("intelephense")
+
+      -- 🔥 Habilitar TODOS (obrigatório no 0.11)
+      vim.lsp.enable("ts_ls")
+      vim.lsp.enable("angularls")
+      vim.lsp.enable("html")
+      vim.lsp.enable("cssls")
+      vim.lsp.enable("jsonls")
+      vim.lsp.enable("intelephense")
     end,
   },
 }
