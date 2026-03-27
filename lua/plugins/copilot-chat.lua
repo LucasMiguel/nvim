940fded15b71b8f6f64345d32e480c3c3f0cb9c8
Lucas Miguel <lucas.miguel@dommus.tec.br>
2026-03-27 12:44:16 -0300

    Ajuste de copilot chat

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -3,11 +3,26 @@ return{{
   dependencies = {
     "zbirenbaum/copilot.lua",
     "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
   },
+  build = "make tiktoken",
   config = function()
+    local function load_instructions()
+      local path = vim.fn.getcwd() .. ".github-copilot/copilot-instructions.md"
+      if vim.fn.filereadable(path) == 1 then
+        return table.concat(vim.fn.readfile(path), "\n")
+      end
+      return ""
+    end
     require("CopilotChat").setup({
       debug = false,
       model="gpt-5-mini",
+      auto_insert_mode = true,
+      picker = "telescope",
+      context = {
+        "Sempre responda em português.",
+        load_instructions(),
+      },
     })
   end
 }}
