ea90826010043fbcb9d8cbae5ae3b714de960c10
Lucas Miguel <lucasoliveira129@hotmail.com>
2026-07-28 15:30:56 -0300

    Ajuste no treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,43 +1,54 @@
-return {{
+local ensure_installed = {
+  "angular",
+  "bash",
+  "c",
+  "css",
+  "csv",
+  "dart",
+  "dockerfile",
+  "html",
+  "javascript",
+  "json",
+  "json5",
+  "lua",
+  "php",
+  "python",
+  "regex",
+  "scss",
+  "sql",
+  "tsx",
+  "typescript",
+  "vim",
+  "vimdoc",
+  "yaml",
+}
+
+return {
 	"nvim-treesitter/nvim-treesitter",
 	branch = "main",
-    lazy = false,
+	lazy = false,
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter").setup({
-          ensure_installed = {
-            "angular",
-            "bash",
-            "c",
-            "css",
-            "csv",
-            "dart",
-            "dockerfile",
-            "html",
-            "javascript",
-            "json",
-            "json5",
-            "lua",
-            "php",
-            "php_only",
-            "python",
-            "regex",
-            "scss",
-            "sql",
-            "tsx",
-            "typescript",
-            "vim",
-            "vimdoc",
-            "yaml",
-          },
-          sync_install = false,
-          auto_install = true,
-          highlight = {
-            enable = true,
-          },
-          indent = {
-            enable = true,
-          },
-        })
+		require("nvim-treesitter").setup()
+
+		local installed = require("nvim-treesitter.config").get_installed("parsers")
+		local missing = vim.tbl_filter(function(lang)
+			return not vim.tbl_contains(installed, lang)
+		end, ensure_installed)
+		if #missing > 0 then
+			require("nvim-treesitter").install(missing)
+		end
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "*",
+			callback = function(args)
+				local lang = vim.treesitter.language.get_lang(args.match) or args.match
+				if not vim.tbl_contains(require("nvim-treesitter.config").get_installed("parsers"), lang) then
+					return
+				end
+				vim.treesitter.start(args.buf, lang)
+				vim.bo[args.buf].indentexpr = "v:lua.vim.treesitter.indentexpr()"
+			end,
+		})
 	end,
-}}
+}
